6e05c82db63de29306ff4f070365b82c52b5b5c2
Ninnka <hennzr@gmail.com>
2017-01-04 15:09:22 +0800

    update

--- get_access_token.php
+++ get_access_token.php
@@ -33,7 +33,7 @@ if($data->time < time() - 7000){
 
 // 根据 openid 获取用户信息
 
-$openid = 'oNcA9w9aFHgLy8k1FR-drzB443EI';
+$openid = 'oNcA9w7GJmypTW02EBXCDgbsEobE';
 $access_token = 'c9zvBpqurygHz2T3Cq-pue7Fr-3ehUnH_Su0EcXIPeQG3STsgF7vbgh9jrkyxFvuTYjCHq_z2uz7cfwwlYiFRxZ6QUcU0zCPUKf1Akxp6mzGMpos5ubX6TJY79x3e5vRTWBcAIAHKP';
 $url_userinfo = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={$access_token}&openid={$openid}&lang=zh_CN";
 
@@ -43,8 +43,9 @@ $obj = json_decode($data);
 // echo $str;
 echo "<table>";
 echo "<tr>
+        <td><img src='{$obj->headimgurl}' style='width: 50px; height: 50px;'/></td>
         <td>{$obj->nickname}</td>
-        <td>".$obj->sex == 1 ? "男" : "女"."</td>
+        <td>".($obj->sex == 1 ? "男" : "女")."</td>
         <td>{$obj->city}</td>
       </tr>";
 echo "</table>";
