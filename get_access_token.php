cf53e84d48c280af5d94ddb7e9bf7353e1880827
Ninnka <hennzr@gmail.com>
2017-01-04 15:00:09 +0800

    update

--- get_access_token.php
+++ get_access_token.php
@@ -1,4 +1,5 @@
 <?php
+$access_token = "";
 $appid = 'wx9e04810f0033f158';
 $secret = 'ff165d3bba903801dd02712c5b57ec8f';
 
@@ -9,7 +10,7 @@ $filepathsae = "saemc://access_token.txt";
 // $filepath = "access_token.txt";
 
 $str = file_get_contents($filepathsae);
-echo $str;
+// echo $str;
 
 // $str = file_get_contents($filepath);
 
@@ -20,6 +21,7 @@ if($data->time < time() - 7000){
   $str = file_get_contents($url);
   $json = json_decode($str);
   $data->access_token = $json->access_token;
+  $access_token = $json->access_token;
   $data->time = time()+7000;
   $str = json_encode($data);
 
@@ -28,3 +30,14 @@ if($data->time < time() - 7000){
 
   // file_put_contents($filepath, $str);
 }
+
+// 根据 openid 获取用户信息
+
+$openid = 'oNcA9w9aFHgLy8k1FR-drzB443EI';
+// $access_token = 'c9zvBpqurygHz2T3Cq-pue7Fr-3ehUnH_Su0EcXIPeQG3STsgF7vbgh9jrkyxFvuTYjCHq_z2uz7cfwwlYiFRxZ6QUcU0zCPUKf1Akxp6mzGMpos5ubX6TJY79x3e5vRTWBcAIAHKP';
+$url_userinfo = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={$access_token}&openid={$openid}&lang=zh_CN";
+
+$data = file_get_contents($url_userinfo);
+$str = json_encode($data);
+
+echo $str;
--- get_union_id.php
+++ get_union_id.php
@@ -1,10 +1,9 @@
 <?php
 $openid = 'oNcA9w9aFHgLy8k1FR-drzB443EI';
 $access_token = 'c9zvBpqurygHz2T3Cq-pue7Fr-3ehUnH_Su0EcXIPeQG3STsgF7vbgh9jrkyxFvuTYjCHq_z2uz7cfwwlYiFRxZ6QUcU0zCPUKf1Akxp6mzGMpos5ubX6TJY79x3e5vRTWBcAIAHKP';
-$url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={$access_token}&openid={$openid}&lang=zh_CN";
+$url_userinfo = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={$access_token}&openid={$openid}&lang=zh_CN";
 
-
-$data = file_get_contents($url);
+$data = file_get_contents($url_userinfo);
 $str = json_encode($data);
 
 echo $str;
