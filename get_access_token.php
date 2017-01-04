d85637e6667f9d50ca3bf10a62f61bbc364b3fa7
Ninnka <hennzr@gmail.com>
2017-01-04 15:04:37 +0800

    update

--- get_access_token.php
+++ get_access_token.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = "";
+// $access_token = "";
 $appid = 'wx9e04810f0033f158';
 $secret = 'ff165d3bba903801dd02712c5b57ec8f';
 
@@ -21,7 +21,7 @@ if($data->time < time() - 7000){
   $str = file_get_contents($url);
   $json = json_decode($str);
   $data->access_token = $json->access_token;
-  $access_token = $json->access_token;
+  // $access_token = $json->access_token;
   $data->time = time()+7000;
   $str = json_encode($data);
 
@@ -34,10 +34,17 @@ if($data->time < time() - 7000){
 // 根据 openid 获取用户信息
 
 $openid = 'oNcA9w9aFHgLy8k1FR-drzB443EI';
-// $access_token = 'c9zvBpqurygHz2T3Cq-pue7Fr-3ehUnH_Su0EcXIPeQG3STsgF7vbgh9jrkyxFvuTYjCHq_z2uz7cfwwlYiFRxZ6QUcU0zCPUKf1Akxp6mzGMpos5ubX6TJY79x3e5vRTWBcAIAHKP';
+$access_token = 'c9zvBpqurygHz2T3Cq-pue7Fr-3ehUnH_Su0EcXIPeQG3STsgF7vbgh9jrkyxFvuTYjCHq_z2uz7cfwwlYiFRxZ6QUcU0zCPUKf1Akxp6mzGMpos5ubX6TJY79x3e5vRTWBcAIAHKP';
 $url_userinfo = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={$access_token}&openid={$openid}&lang=zh_CN";
 
 $data = file_get_contents($url_userinfo);
-$str = json_encode($data);
+$obj = json_decode($data);
 
-echo $str;
+// echo $str;
+echo "<table>";
+echo "<tr>
+        <td>{$obj->nickname}</td>
+        <td>".$obj->sex == 1 ? "男" : "女"."</td>
+        <td>{$obj->city}</td>
+      </tr>";
+echo "</table>";
