32a9d86a2a39de759ab14143d35218f6e471e2f8
Ninnka <hennzr@gmail.com>
2017-01-04 11:43:31 +0800

    update

--- get_access_token.php
+++ get_access_token.php
@@ -4,9 +4,18 @@ $secret = 'ff165d3bba903801dd02712c5b57ec8f';
 
 // 获取access_token地址
 $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$appid}&secret={$secret}";
+$filepath = "access_token.txt";
+$str = file_get_contents($filepath);
+$data = json_decode($str);
 
-$str = file_get_contents($url);
-$json = json_decode($str);
+// $access_token = $data->access_token;
+// echo $access_token;
 
-$access_token = $json->access_token;
-echo $access_token;
+if($data->time < time()){
+  $str = file_get_contents($url);
+  $json = json_decode($str);
+  $data->access_token = $json->access_token;
+  $data->time = time()+7000;
+  $str = json_encode($data);
+  file_put_contents($filepath, $str);
+}
