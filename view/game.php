15f1bbb248bdd38a8e7c593598576e388c852c14
Ninnka <hennzr@gmail.com>
2017-01-05 21:23:29 +0800

    update user info 7

--- view/game.php
+++ view/game.php
@@ -239,7 +239,6 @@ $signPackage = $jssdk->GetSignPackage();
 
     <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
     <script type="text/javascript">
-            var username = "";
           wx.config({
     	    debug: false,
     	    appId: '<?php echo $signPackage["appId"];?>',
@@ -268,9 +267,10 @@ $signPackage = $jssdk->GetSignPackage();
     	      }
     	  	});
 
-            username = '<?php echo $obj->nickname;?>';
-
     	  });
+
+          var un = '<?php echo $obj->nickname;?>';
+          console.log("un", un);
 
 
     </script>
