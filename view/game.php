e73d49edbfa9979ede89878304d8d6395aae23f3
Ninnka <hennzr@gmail.com>
2017-01-05 17:03:47 +0800

    add share version-0.2.3

--- view/game.php
+++ view/game.php
@@ -239,7 +239,7 @@ $signPackage = $jssdk->GetSignPackage();
     <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
     <script type="text/javascript">
         wx.config({
-            debug: false,
+            debug: true,
             appId: '<?php echo $signPackage["appId"];?>',
             timestamp: <?php echo $signPackage["timestamp"];?>,
             nonceStr: '<?php echo $signPackage["nonceStr"];?>',
@@ -259,7 +259,6 @@ $signPackage = $jssdk->GetSignPackage();
         result_share.addEventListener("click", function () {
           // alert("share");
           var headimgurl = '<?php echo $obj->headimgurl;?>';
-          var nickname = '<?php echo $obj->nickname;?>';
           alert("headimgurl: "+headimgurl);
 
         //   wx.onMenuShareAppMessage({
@@ -278,16 +277,6 @@ $signPackage = $jssdk->GetSignPackage();
         //       //   console.log("cancel");
         //     }
         //   });
-            wx.getLocation({
-              type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-              success: function (res) {
-                  var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                  var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                  var speed = res.speed; // 速度，以米/每秒计
-                  var accuracy = res.accuracy; // 位置精度
-                  alert(latitude,longitude,speed,accuracy);
-              }
-            });
         });
 
     </script>
