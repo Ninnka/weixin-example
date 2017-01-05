c2c4a726c6fc94986a88f98680e3bf7574af8395
Ninnka <hennzr@gmail.com>
2017-01-05 10:19:50 +0800

    update

--- sample.php
+++ sample.php
@@ -72,7 +72,7 @@ $signPackage = $jssdk->GetSignPackage();
     signature: '<?php echo $signPackage["signature"];?>',
     jsApiList: [
       // 所有要调用的 API 都要加到这个列表中
-      "startRecord", "stopRecord", "onVoiceRecordEnd"
+      // "startRecord", "stopRecord", "onVoiceRecordEnd"
     ]
   });
   wx.ready(function () {
