7536dd3843212382b253cd863212193a614ef974
Ninnka <hennzr@gmail.com>
2017-01-05 10:00:59 +0800

    update

--- sample.php
+++ sample.php
@@ -33,7 +33,7 @@ $signPackage = $jssdk->GetSignPackage();
     signature: '<?php echo $signPackage["signature"];?>',
     jsApiList: [
       // 所有要调用的 API 都要加到这个列表中
-      "getLocation"
+      "getNetworkType"
     ]
   });
   wx.ready(function () {
@@ -65,18 +65,25 @@ $signPackage = $jssdk->GetSignPackage();
     //   }
     // });
 
-    wx.getLocation({
-      type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-      success: function (res) {
-          var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-          var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-          var speed = res.speed; // 速度，以米/每秒计
-          var accuracy = res.accuracy; // 位置精度
-          alert(latitude,longitude,speed,accuracy);
-      }
-    });
+    // wx.getLocation({
+    //   type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+    //   success: function (res) {
+    //       var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+    //       var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+    //       var speed = res.speed; // 速度，以米/每秒计
+    //       var accuracy = res.accuracy; // 位置精度
+    //       alert(latitude,longitude,speed,accuracy);
+    //   }
+    // });
 
     // wx.showOptionMenu();
+
+    wx.getNetworkType({
+      success: function (res) {
+          var networkType = res.networkType; // 返回网络类型2g，3g，4g，wifi
+          alert(networkType);
+      }
+    });
   });
 </script>
 </html>
